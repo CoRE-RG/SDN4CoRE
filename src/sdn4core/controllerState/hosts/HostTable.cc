7b4487b8109b91a6e76c82a790ab612d7b771f55
Timo Haeckel <timo.haeckel@haw-hamburg.de>
2022-02-01 17:46:34 +0100

    add first implementation of host table

--- src/sdn4core/controllerState/hosts/HostTable.cc
+++ src/sdn4core/controllerState/hosts/HostTable.cc
@@ -13,16 +13,186 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#include "HostTable.h"
+#include "sdn4core/controllerState/hosts/HostTable.h"
+
+#include "core4inet/utilities/customWatch.h"
+
+using namespace std;
+using namespace inet;
+
+namespace SDN4CoRE {
 
 Define_Module(HostTable);
 
-void HostTable::initialize()
-{
-    // TODO - Generated method body
+std::ostream& operator<<(std::ostream& os, const HostTable::HostEntry& entry) {
+    os << "{";
+    os << "nodeName=" << entry.nodeName;
+    os << ", macAddress=" << entry.macAddress;
+    os << ", vid=" << entry.vid;
+    os << ", ipAddresses={";
+    bool first = true;
+    for (auto & address : entry.ipAddresses) {
+        if (first) {
+            first = false;
+        } else {
+            os << ", ";
+        }
+        os << address.str();
+    }
+    os << "}";
+    os << ", switchInfo=" << entry.switchInfo->getMacAddress(); // todo add full swinfo as string?
+    os << ", portno=" << entry.portno;
+    os << ", learned=" << entry.learned;
+    os << ", insertionTime=" << entry.insertionTime;
+    os << ", lastUpdated=" << entry.lastUpdated;
+    os << "}";
+    return os;
 }
 
-void HostTable::handleMessage(cMessage *msg)
-{
-    // TODO - Generated method body
+HostTable::HostTable() {
 }
+
+HostTable::~HostTable() {
+    for (auto & elem : hosts)
+        delete elem.second;
+}
+
+HostTable::HostList HostTable::getHostsForMacAddress(
+        inet::MACAddress& address) {
+    Enter_Method
+    ("HostTable::getHostsForMacAddress()");
+    HostList found;
+    for (auto& table : hosts) {
+        auto iter = table.second->find(address);
+        if (iter != table.second->end()) {
+            if (iter->second.lastUpdated + agingTime <= simTime()) {
+                // don't use (and throw out) aged entries
+                EV << "Ignoring and deleting aged entry: " << iter->first
+                          << " --> port" << iter->second.portno << "\n";
+                table.second->erase(iter);
+            } else if (iter->first == address) {
+                found.push_back(iter->second);
+            }
+        }
+    }
+    return found;
+}
+
+HostTable::HostEntry* HostTable::getHostForMacAddressAndVlan(
+        inet::MACAddress& address, unsigned int vid) {
+    Enter_Method
+    ("HostTable::getHostsForMacAddressAndVlan()");
+    HostMap *table = getTableForVid(vid);
+    // VLAN ID vid does not exist
+    if (table == nullptr) {
+        return nullptr;
+    }
+    auto iter = table->find(address);
+    if (iter == table->end()) {
+        // not found
+        return nullptr;
+    }
+    if (iter->second.lastUpdated + agingTime <= simTime()) {
+        // don't use (and throw out) aged entries
+        EV << "Ignoring and deleting aged entry: " << iter->second.macAddress << " --> port"
+                  << iter->second.portno << "\n";
+        table->erase(iter);
+        return nullptr;
+    }
+    return &(iter->second);
+}
+
+int HostTable::getPortForHostMacAddress(inet::MACAddress& address,
+        unsigned int vid) {
+    Enter_Method
+    ("HostTable::getPortForHostMacAddress()");
+    HostMap *table = getTableForVid(vid);
+    // VLAN ID vid does not exist
+    if (table == nullptr) {
+        return -1;
+    }
+    auto iter = table->find(address);
+    if (iter == table->end()) {
+        // not found
+        return -1;
+    }
+    if (iter->second.lastUpdated + agingTime <= simTime()) {
+        // don't use (and throw out) aged entries
+        EV << "Ignoring and deleting aged entry: " << iter->first << " --> port"
+                  << iter->second.portno << "\n";
+        table->erase(iter);
+        return -1;
+    }
+    return iter->second.portno;
+}
+
+void HostTable::removeAgedEntriesFromVlan(unsigned int vid) {
+    HostMap *table = getTableForVid(vid);
+    if (table != nullptr) {
+        removeAgedEntriesFromHostMap(table);
+    }
+}
+
+void HostTable::removeAgedEntriesFromAllVlans() {
+    for (auto& table : hosts) {
+        removeAgedEntriesFromHostMap(table.second);
+    }
+}
+
+void HostTable::removeAgedEntriesIfNeeded() {
+    simtime_t now = simTime();
+    if (now >= lastPurge + 1) {
+        removeAgedEntriesFromAllVlans();
+    }
+    lastPurge = now;
+}
+
+void HostTable::clearTable() {
+    for (auto & elem : hosts) {
+        delete elem.second;
+    }
+    hosts.clear();
+}
+
+void HostTable::setAgingTime(simtime_t agingTime) {
+    this->agingTime = agingTime;
+}
+
+void HostTable::resetDefaultAging() {
+    agingTime = par("agingTime");
+}
+
+void HostTable::initialize() {
+    agingTime = par("agingTime");
+    lastPurge = SIMTIME_ZERO;
+    WATCH_MAPMAP(hosts);
+}
+
+void HostTable::handleMessage(cMessage* msg) {
+    throw cRuntimeError("This module doesn't process messages");
+}
+
+HostTable::HostMap* HostTable::getTableForVid(unsigned int vid) {
+    auto iter = hosts.find(vid);
+    if (iter != hosts.end()) {
+        return iter->second;
+    }
+    return nullptr;
+}
+
+void HostTable::removeAgedEntriesFromHostMap(HostMap* table) {
+    if (table == nullptr) {
+        return;
+    }
+    for (auto iter = table->begin(); iter != table->end(); ) {
+        if (iter->second.lastUpdated + agingTime <= simTime()) {
+            // don't use (and throw out) aged entries
+            EV << "Deleting aged entry: " << iter->first << " --> port"
+                      << iter->second.portno << "\n";
+            table->erase(iter);
+        }
+    }
+}
+
+} /*end namespace SDN4CoRE*/
+
--- src/sdn4core/controllerState/hosts/HostTable.h
+++ src/sdn4core/controllerState/hosts/HostTable.h
@@ -18,10 +18,11 @@
 
 #include <omnetpp.h>
 // STD
-#include <vector>
+#include <list>
+#include <map>
 // INET
 #include "inet/linklayer/common/MACAddress.h"
-#include "inet/networklayer/contract/ipv4/IPv4Address.h"
+#include "inet/networklayer/common/L3Address.h"
 //openflow
 #include "openflow/openflow/controller/Switch_Info.h"
 
@@ -36,6 +37,7 @@ namespace SDN4CoRE {
  */
 class HostTable : public cSimpleModule
 {
+public:
     /**
      * Representation of a HostTable entry.
      * Contains the known MAC and IP Addresses, VLAN IDs, switch and port.
@@ -43,7 +45,7 @@ class HostTable : public cSimpleModule
     struct HostEntry {
         std::string nodeName = "UNKNOWN";
         inet::MACAddress macAddress; // VLAN ID
-        std::vector<unsigned int> vlanIds = {0};
+        unsigned int vid = 0;
         std::vector<inet::L3Address> ipAddresses;
         openflow::Switch_Info* switchInfo = nullptr;
         int portno = -1; // Input port
@@ -53,13 +55,92 @@ class HostTable : public cSimpleModule
 
         HostEntry() : insertionTime(simTime()), lastUpdated(simTime()) {}
     };
+    friend std::ostream& operator<<(std::ostream& os, const HostEntry& entry);
+    struct MAC_compare
+    {
+        bool operator()(const MACAddress& u1, const MACAddress& u2) const { return u1.compareTo(u2) < 0; }
+    };
+    typedef std::list<HostEntry> HostList;
+    typedef std::map<inet::MACAddress, HostEntry, MAC_compare> HostMap;
+    typedef std::map<unsigned int, HostMap *> HostVLANMap;
 
-  protected:
-    virtual void initialize();
-    virtual void handleMessage(cMessage *msg);
+public:
+    HostTable();
+    ~HostTable();
 
-  protected:
-    std::vector<HostEntry>
+    /**
+     * TODO implement lookup for IPs
+     * @param address
+     * @return
+     */
+    virtual HostList getHostsForMacAddress(inet::MACAddress& address);
+
+    /**
+     * TODO implement lookup for IPs
+     * @param address
+     * @return
+     */
+    virtual HostEntry* getHostForMacAddressAndVlan(inet::MACAddress& address, unsigned int vid);
+
+    /**
+     * TODO implement lookup for IPs
+     * @param address
+     * @return
+     */
+    virtual int getPortForHostMacAddress(inet::MACAddress& address, unsigned int vid = 0);
+
+    /**
+     * TODO Specify interface for table insertion!
+     */
+
+    /**
+     * @brief Remove aged entries from a specified VLAN
+     */
+    virtual void removeAgedEntriesFromVlan(unsigned int vid = 0);
+    /**
+     * @brief Remove aged entries from all VLANs
+     */
+    virtual void removeAgedEntriesFromAllVlans();
+
+    /*
+     * It calls removeAgedEntriesFromAllVlans() if and only if at least
+     * 1 second has passed since the method was last called.
+     */
+    virtual void removeAgedEntriesIfNeeded();
+
+    /**
+     * For lifecycle: clears all entries from the vlanAddressTable.
+     */
+    virtual void clearTable();
+
+    /**
+     * Set the agingTime of host entries.
+     * @param agingTime the aging time to set.
+     */
+    virtual void setAgingTime(simtime_t agingTime);
+
+    /**
+     * Reset the agingTime to the one specified in the module parameter.
+     */
+    virtual void resetDefaultAging();
+
+protected:
+
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+
+    /**
+     * @brief Returns a Host Table for a specified VLAN ID
+     */
+    HostMap *getTableForVid(unsigned int vid);
+
+    void removeAgedEntriesFromHostMap (HostMap* table);
+
+protected:
+
+    simtime_t agingTime;    // Max idle time for address table entries
+    simtime_t lastPurge;    // Time of the last call of removeAgedEntriesFromAllVlans()
+    HostVLANMap hosts;
 
 
 };
