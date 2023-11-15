44229aaf1cbccc82234cd156acc9a9b11d9c0ef6
Timo Haeckel <timo.haeckel@haw-hamburg.de>
2023-11-15 14:28:45 +0100

    create unique stream Id for someip flows

--- src/sdn4core/controllerApps/someipSD/SomeipSDControllerApp.cc
+++ src/sdn4core/controllerApps/someipSD/SomeipSDControllerApp.cc
@@ -35,6 +35,8 @@
 //CoRE4INET
 #include "core4inet/utilities/customWatch.h"
 #include "core4inet/utilities/HelperFunctions.h"
+//SOA4CoRE
+#include "soa4core/utility/comfortFunctions.h"
 
 using namespace inet;
 using namespace std;
@@ -637,7 +639,7 @@ void SomeipSDControllerApp::reserveRessourcesForSubscription(
         Subscription& sub, TopologyManagement::Route route) {
     // the serviceId conversion is okay since the service ID is actually uint16_t in the messages
     uint16_t serviceId = sub.service.getServiceId();
-    int instanceId = sub.service.getInstanceId();
+    uint16_t instanceId = sub.service.getInstanceId();
     IPv4Address ip_src = sub.getSrcHostIp();
     IPv4Address ip_dst = sub.consumerEndpoint.getIpv4Address();
     MACAddress mac_dest;
@@ -656,14 +658,9 @@ void SomeipSDControllerApp::reserveRessourcesForSubscription(
     uint16_t l1frameSize = calculateL1Framesize(sub.consumerEndpoint.getL4Protocol(), ressourceConfig->getMaxPayload());
     double interval_cmi_ratio = ressourceConfig->getMinInterval() / getIntervalForClass(SR_CLASS::A);
     uint16_t frameSize = l1frameSize / interval_cmi_ratio;
-    // we need a unique stream ID per flow which is not trivial for UNICAST
-    // using the service id is not enough when one service has multiple !UNICAST! destination nodes
-    // using the destination MAC is not enough when multiple services have the same !UNICAST! destination addresses
-    // the combination of the two, however, is unique
-    // we use a 64 bit value with 2 highest bytes occupied by serviceId and the lower 6 by the MAC address.
-    uint64 macDestAsInt = mac_dest.getInt(); // from INET::MACAddress: 6*8=48 bit address, lowest 6 bytes are used, highest 2 bytes are always zero
-    uint64_t shiftedServiceId = ((uint64_t) serviceId) << 48;// shift to occupy the two highest 2 bytes
-    uint64 streamId = macDestAsInt + shiftedServiceId;
+    // -- not unique if multiple instances of a service exist and are subscribed by the same destination
+    // uint64_t streamId = buildStreamIDForService(serviceId, mac_dest)
+    uint64_t streamId = buildStreamIDForService(serviceId, instanceId, ip_dst);
     for(SwitchPort& switchPort : route)
     {// for each hop
         // 1. update talker in sr table
